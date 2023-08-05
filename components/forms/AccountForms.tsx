61f7c4fab6eb0c2fdad68cb7343c99c77ee8234b
saurabh singh <singhsaurabh1905@gmail.com>
2023-08-05 20:36:48 +0530

    stucked

--- components/forms/AccountForms.tsx
+++ components/forms/AccountForms.tsx
@@ -17,7 +17,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { UserValidation } from "@/lib/validations/user";
 import * as z from "zod";
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
+import { Textarea } from "../ui/textarea";
+import { isBase64Image } from "@/lib/utils";
+import {useUploadThing} from '@/lib/uploadthing'
+import { updateUser } from "@/lib/actions/user.actions";
+import {usePathname,useRouter} from 'next/navigation'
 
 interface Props {
   user: {
@@ -32,28 +37,73 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
+
+const [files, setFiles] = useState<File[]>([])
+const {startUpload } = useUploadThing("media")
+
+const router = useRouter()
+const pathname = usePathname()
+
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
-      profile_photo: "",
-      name: "",
-      username: "",
-      bio: "",
+      profile_photo: user?.image || "",
+      name: user?.name || "",
+      username: user?.username || "",
+      bio: user?.bio || "",
     },
   });
 
   const handleImage = (
-    e: ChangeEvent,
+    e: ChangeEvent<HTMLInputElement>,
     fieldChange: (value: string) => void
   ) => {
     e.preventDefault();
+
+    const fileReader = new FileReader();
+
+    if(e.target.files && e.target.files.length>0){
+      const file = e.target.files[0] ;
+
+      setFiles(Array.from(e.target.files))
+
+      if(!file.type.includes('image')) return ;
+
+      fileReader.onload= async (event)=> {
+        const imageDataUrl = event.target?.result?.toString() || '';
+
+        fieldChange(imageDataUrl)
+      }
+      fileReader.readAsDataURL(file)
+    }
   };
 
-  function onSubmit(values: z.infer<typeof UserValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+ const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+const blob = values.profile_photo
+
+const hasImageChanged = isBase64Image(blob);
+
+if(hasImageChanged){
+  const imgRes = await startUpload(files)
+
+  if(imgRes && imgRes[0].fileUrl){
+    values.profile_photo = imgRes[0].fileUrl ;
   }
+}
+
+//HERE CALL THE BACKEND FUNCTION TO UPDATE THE USER PROFILE
+await updateUser({
+  userId:user.id,
+ username: values.username,
+  name:values.name,
+  bio:values.bio,
+ image:values.profile_photo,
+  path:pathname
+}
+
+)
+
+}
   return (
     <Form {...form}>
       <form
@@ -65,7 +115,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
           name="profile_photo"
           render={({ field }) => (
             <FormItem className="flex items-center gap-4">
-              <FormLabel className="account-form-image-label">
+              <FormLabel className="account-form_image-label">
                 {field.value ? (
                   <Image
                     src={field.value}
@@ -101,12 +151,12 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
           control={form.control}
           name="name"
           render={({ field }) => (
-            <FormItem className="flex items-center gap-3 w-full">
+            <FormItem className="flex flex-col gap-3 w-full">
               <FormLabel className="text-base-semibold text-light-2">Name</FormLabel>
-              <FormControl className="flex-1 text-base-semibold text-gray-200">
+              <FormControl >
                 <Input
                  type="text"
-                  placeholder="Upload a photo"
+                  placeholder="Name"
                   className="account-form_input no-focus"
                   {...field}
                 />
@@ -118,15 +168,15 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
           control={form.control}
           name="username"
           render={({ field }) => (
-            <FormItem className="flex items-center gap-3 w-full">
-              <FormLabel className="text-base-semibold text-light-2">Username</FormLabel>
-              <FormControl className="flex-1 text-base-semibold text-gray-200">
-                <Input
-                 type="text"
-                  placeholder="Username"
-                  className="account-form_input no-focus"
-                  {...field}
-                />
+            <FormItem className="flex flex-col gap-3 w-full">
+            <FormLabel className="text-base-semibold text-light-2">Username</FormLabel>
+            <FormControl >
+              <Input
+               type="text"
+                placeholder="Username"
+                className="account-form_input no-focus"
+                {...field}
+              />
               </FormControl>
             </FormItem>
           )}
@@ -135,20 +185,20 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
           control={form.control}
           name="bio"
           render={({ field }) => (
-            <FormItem className="flex items-center gap-3 w-full">
-              <FormLabel className="text-base-semibold text-light-2">Your Bio</FormLabel>
-              <FormControl className="flex-1 text-base-semibold text-gray-200">
-                <Textarea
-               rows={10}
-                  placeholder="Write about you "
-                  className="account-form_input no-focus"
-                  {...field}
-                />
+            <FormItem className="flex flex-col gap-3 w-full">
+            <FormLabel className="text-base-semibold text-light-2">Your Bio</FormLabel>
+            <FormControl >
+              <Textarea
+               
+                placeholder="Write about you"
+                className="account-form_input no-focus"
+                {...field}
+              />
               </FormControl>
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button type="submit" className="bg-primary-500">Submit</Button>
       </form>
     </Form>
   );
