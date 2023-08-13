81ccd6f9c04ac65750092bb47cec4e53547acfff
saurabh singh <singhsaurabh1905@gmail.com>
2023-08-13 09:53:35 +0530

    commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,40 @@
 //app/page.tsx
 
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { User } from "lucide-react";
+import { currentUser } from "@clerk/nextjs";
+import ThreadCard from "@/components/cards/ThreadCard";
 
 export default async function Home() {
+  const result = await fetchPosts(1, 30);
+  // console.log(result)
+  const user = await currentUser();
 
-  const result = await fetchPosts()
   return (
     <>
-
       <h1 className="head-text text-left"> Home </h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="no-result">No Threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+               <ThreadCard
+               key={post._id}
+               id={post._id}
+               currentUserId={user?.id || ""}
+               parentId={post.parentId}
+               content={post.text}
+               author={post.author}
+               community={post.community}
+               createdAt={post.createdAt}
+               comments={post.children}
+             />
+            ))}
+          </>
+        )}
+      </section>
     </>
-  )
+  );
 }
